ed3f4420ae9e66d397243937fb1e3d41df3091ba
jmacj <jm.nz0723@gmail.com>
2017-11-09 12:42:39 +0800

    modal update

---
+++ application/models/Card_Tag_model.php
@@ -0,0 +1,65 @@
+<?php
+
+class Card_Tag_model extends CI_Model {
+
+
+	public function get_id($name) {
+		
+		return $this->db->select('id')
+			->from('tags')
+			->where('name', $name)
+			->get()
+			->row()->id;
+	}
+
+
+	public function insert($id, $updated_tags) {
+		
+		$tags = array_column($this->db->get('tags')->result_array(), 'name');
+
+		foreach($updated_tags as $updated_tag) {
+			
+			if(!in_array($updated_tag, $tags)) {
+			
+				$this->db->insert('tags', [
+					'name' => $updated_tag
+				]);
+			}
+
+			if(!in_array($updated_tag, $this->get($id))) {
+
+				$this->db->insert('cards_tagging', [
+					'tag_id'  => $this->get_id($updated_tag),
+					'card_id' => $id
+				]);
+			}
+		}
+	}
+
+
+	public function update($id, $updated_tags) {
+		
+		$this->insert($id, $updated_tags);
+		
+		foreach($this->get($id) as $old_tag)
+			if(!in_array($old_tag, $updated_tags))
+				$this->db->delete('cards_tagging', ['card_id' => $id, 'tag_id' => $this->get_id($old_tag)]);
+	}
+
+
+	public function get($id) {
+
+		$names = [];
+		$tags  =  $this->db->select('name')
+			->from('tags')
+			->join('cards_tagging', 'cards_tagging.tag_id = tags.id')
+			->where('cards_tagging.card_id', $id)
+			->get()
+			->result();
+
+		foreach ($tags as $tag)
+			$names[] = $tag->name;
+		
+		return $names;
+	}
+}
