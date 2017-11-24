73265810ddb6d3cfe138c33ee91e9ba49a020280
Christian Jordan Dalan <cjdalan@gmail.com>
2017-11-24 20:10:11 +0800

    Fix redirect URL

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -11,7 +11,7 @@ class Views extends CI_Controller {
 
         if (!$this->user_model->is_login()) {
         
-            redirect('http://localhost/main');
+            redirect('http://payakapps.com/users/login');
         }
     }
 
