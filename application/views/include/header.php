49c3df180f3e18256442036544e122222a338560
aereneth <zerify@gmail.com>
2018-02-20 11:55:45 +0800

    Added mini-tutorial

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -31,7 +31,12 @@
 
             <ul class="sidebar-menu">		
 
-                
+                <li class="">
+                    <a class="font-weight-bold text-warning" href="#tutorialModal" data-toggle="modal">
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                            <span>Get Started</span>
+                    </a>
+                </li>
 
                 <li class="">
                     <a class="task-create" href="#searchTaskModal" data-toggle="modal">
