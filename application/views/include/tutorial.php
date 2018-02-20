bf22dc3753179938bbdf7bc4ef017e163eca00fd
aereneth <zerify@gmail.com>
2018-02-20 14:25:44 +0800

    Updated tutorial

--- application/views/include/tutorial.php
+++ application/views/include/tutorial.php
@@ -1,61 +1,65 @@
 <div id="tutorialModal" class="modal fade" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-lg" role="document">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="card border-0 rounded-0">
-                        <img src="<?= base_url('assets/tutorial/welcome.png') ?>" alt="Sidebar" class="rounded-0 card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Welcome PayakApps' Bulletin Board</h5>
-                            <p class="card-text">This is where all the important stuffs are!</p>
+        <div class="modal-content" style="box-shadow: 0 0 10px rgba(0,0,0,0.2);">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/welcome.png') ?>" alt="Sidebar" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Welcome PayakApps' Bulletin Board</h5>
+                                <p class="card-text">This is where all the important stuffs are!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/announcements.png') ?>" alt="Dashboard" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Announcements</h5>
+                                <p class="card-text">All the announcements made will be shown below.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/create.png') ?>" alt="Sidebar" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Creating Announcement</h5>
+                                <p class="card-text">Creating an announcement is as easy as 1-2-3! Just give it a title and description. You may also customize each card according to your own taste!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/update.png') ?>" alt="Account" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Updating Announcements</h5>
+                                <p class="card-text">You may also update an announcement by selecting an announcement and clicking "Edit Post" on the dropdown near the announcement title.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="card border-0 rounded-0">
+                            <img src="<?= base_url('assets/tutorial/search.png') ?>" alt="Account" class="rounded-0 card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Search Announcements</h5>
+                                <p class="card-text">You may also search announcements by clicking "Search" on the sidebar.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <div class="card border-0 rounded-0">
-                        <img src="<?= base_url('assets/tutorial/announcements.png') ?>" alt="Dashboard" class="rounded-0 card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Announcements</h5>
-                            <p class="card-text">All the announcements made will be shown below.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card border-0 rounded-0">
-                        <img src="<?= base_url('assets/tutorial/create.png') ?>" alt="Sidebar" class="rounded-0 card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Creating Announcement</h5>
-                            <p class="card-text">Creating an announcement is as easy as 1-2-3! Just give it a title and description. You may also customize each card according to your own taste!</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card border-0 rounded-0">
-                        <img src="<?= base_url('assets/tutorial/update.png') ?>" alt="Account" class="rounded-0 card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Updating Announcements</h5>
-                            <p class="card-text">You may also update an announcement by selecting an announcement and clicking "Edit Post" on the dropdown near the announcement title.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card border-0 rounded-0">
-                        <img src="<?= base_url('assets/tutorial/search.png') ?>" alt="Account" class="rounded-0 card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Search Announcements</h5>
-                            <p class="card-text">You may also search announcements by clicking "Search" on the sidebar.</p>
-                        </div>
-                    </div>
-                </div>
+                <a class="carousel-control-prev text-dark" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <i class="fa fa-angle-left fa-3x"></i>
+                    <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next text-dark" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <i class="fa fa-angle-right fa-3x"></i>
+                    <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev text-dark" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next text-dark" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
     </div>
 </div>
